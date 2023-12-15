05c6e81a374e8d2d86cb54f9e74197972ebf46d9
hitashaa <145817252+hitashaa@users.noreply.github.com>
2023-12-14 19:10:40 -0500

    Fixed IncomeModel.js

--- backend/models/IncomeModel.js
+++ backend/models/IncomeModel.js
@@ -1,12 +1,4 @@
-const mongoose = require('mongoose');
 
-
-const IncomeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
     },
     amount: {
         type: Number,
